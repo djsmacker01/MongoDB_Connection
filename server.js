7dc795c887fbb5fee48d032adb1f53ed311a46f0
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-07 23:52:21 +0100

    update server

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 const PORT = 8000
 
-const url = 'mongodb+srv://djsmacker01:Fabulous01@cluster0.dnlsvzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// const url = 'mongodb+srv://djsmacker01:Fabulous01@cluster0.dnlsvzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 //  const url = 'mongodb+srv://djsmacker01:<password>@cluster0.dnlsvzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const url = 'mongodb+srv://djsmacker:Fabulous01@cluster0.wkc3lvf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 async function connect() {
     try {
         await mongoose.connect(url);
